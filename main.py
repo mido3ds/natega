c0afe05e7fc4d60c5be0bd2f0d801d579020d072
Mahmoud Adas <mido3ds@gmail.com>
2017-06-26 09:41:42 +0200

    refactor

--- main.py
+++ main.py
@@ -2,13 +2,18 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+
 def get_page():
     return urlopen(url='http://www.results.eng.cu.edu.eg/').read()
 
+
 def parse_page(page):
     return BeautifulSoup(page, 'html.parser').find(id='td44')
+
 
 def is_marked(soup):
     return soup.find('a') is not None
 
 natega_appeared = is_marked(parse_page(get_page()))
+
+##########################################################################
