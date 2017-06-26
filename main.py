2b3294a729a9a785925e413c9ce04f2a2906581b
Mahmoud Adas <mido3ds@gmail.com>
2017-06-26 09:40:46 +0200

    finished main logic

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import bs4
+#!/usr/bin/env python3
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
-bs4.BeautifullSoup
+def get_page():
+    return urlopen(url='http://www.results.eng.cu.edu.eg/').read()
+
+def parse_page(page):
+    return BeautifulSoup(page, 'html.parser').find(id='td44')
+
+def is_marked(soup):
+    return soup.find('a') is not None
+
+natega_appeared = is_marked(parse_page(get_page()))
